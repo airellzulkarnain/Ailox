d97617e3f33542246b3be97064f21b5e88fabf53
airellzulkarnain <airell.zulkarnain@gmail.com>
2022-04-11 14:06:54 +0700

    login, cancel on using identity.

---
+++ ailox-wms/ailox-wms/Pages/Index.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using ailox_wms.Data;
+using ailox_wms.Models;
+
+namespace ailox_wms.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly AiloxContext _context;
+
+        public IndexModel(ILogger<IndexModel> logger, AiloxContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+        public User User { get; set; }
+        public async Task<IActionResult> OnGetAsync()
+        {
+            string username = HttpContext.Session.GetString("_username");
+            if (username == null)
+            {
+                return RedirectToPage("./Login");
+            }
+            User = await _context.Users.AsNoTracking().SingleAsync(s => s.username == username);
+            return Page();
+        }
+    }
+}
---
+++ ailox-wms/ailox-wms/wwwroot/js/site.js
@@ -0,0 +1,13 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+let overlay = document.createElement("div");
+overlay.className = "overlay";
+overlay.addEventListener("click", () => {
+    document.querySelector(".overlay").remove();
+});
+document.querySelector("[logout]").addEventListener("click", () => {
+    document.body.appendChild(overlay);
+    document.body.appendChild();
+});
